e6e0c673a4a872dc2488ee30d545c6f14856d0fc
Cybo1927 <39284087+Cybo1927@users.noreply.github.com>
2019-11-17 21:43:39 +0000

    Update Scriptlets.js

--- Advanced/Scriptlets.js
+++ Advanced/Scriptlets.js
@@ -15,9 +15,109 @@
     }
 })();
 
-// element-clicker-onload
+// element-clicker-onload (No Delay)
 /// eclick.js
 /// alias ec.js
+(() => {
+    'use strict';
+    let selector = '{{1}}';
+    if (selector === '' || selector === '{{1}}') {
+        return;
+}
+    let click = () => {
+        let element = document.querySelector(selector);
+        element.click();
+    };
+    if (document.readyState === 'complete') {
+        click();
+    } else {
+        addEventListener('DOMContentLoaded', click);
+    }
+})();
+
+// element-clicker-onload (1 Second Delay)
+/// eclick1.js
+/// alias ec1.js
+(() => {
+    'use strict';
+    let selector = '{{1}}';
+    if (selector === '' || selector === '{{1}}') {
+        return;
+}
+    let click = () => {
+        let element = document.querySelector(selector);
+        element.click();
+    };
+    if (document.readyState === 'complete') {
+        click();
+    } else {
+        addEventListener('DOMContentLoaded', setTimeout(click, 1000));
+    }
+})();
+
+// element-clicker-onload (2 Second Delay)
+/// eclick2.js
+/// alias ec2.js
+(() => {
+    'use strict';
+    let selector = '{{1}}';
+    if (selector === '' || selector === '{{1}}') {
+        return;
+}
+    let click = () => {
+        let element = document.querySelector(selector);
+        element.click();
+    };
+    if (document.readyState === 'complete') {
+        click();
+    } else {
+        addEventListener('DOMContentLoaded', setTimeout(click, 2000));
+    }
+})();
+
+// element-clicker-onload (3 Second Delay)
+/// eclick3.js
+/// alias ec3.js
+(() => {
+    'use strict';
+    let selector = '{{1}}';
+    if (selector === '' || selector === '{{1}}') {
+        return;
+}
+    let click = () => {
+        let element = document.querySelector(selector);
+        element.click();
+    };
+    if (document.readyState === 'complete') {
+        click();
+    } else {
+        addEventListener('DOMContentLoaded', setTimeout(click, 3000));
+    }
+})();
+
+// element-clicker-onload (4 Second Delay)
+/// eclick4.js
+/// alias ec4.js
+(() => {
+    'use strict';
+    let selector = '{{1}}';
+    if (selector === '' || selector === '{{1}}') {
+        return;
+}
+    let click = () => {
+        let element = document.querySelector(selector);
+        element.click();
+    };
+    if (document.readyState === 'complete') {
+        click();
+    } else {
+        addEventListener('DOMContentLoaded', setTimeout(click, 4000));
+    }
+})();
+
+// element-clicker-onload (5 Second Delay)
+/// eclick5.js
+/// alias ec5.js
 (() => {
     'use strict';
     let selector = '{{1}}';
