2afba574db8c8bcbf81bb5c74025d78eee703843
Cybo1927 <39284087+Cybo1927@users.noreply.github.com>
2019-11-17 02:35:36 +0000

    Update and rename Scriptlets.txt to Scriptlets.js

--- Advanced/Scriptlets.js
+++ Advanced/Scriptlets.js
@@ -1,7 +1,16 @@
 // Based on https://raw.githubusercontent.com/uBlock-user/uBO-Scriptlets/master/scriptlets.txt
 // https://raw.githubusercontent.com/NanoAdblocker/NanoCore2/master/src/snippets.js
-// https://raw.githubusercontent.com/uBlock-user/uBO-Scriptlets/master/scriptlets.txt
+// https://raw.githubusercontent.com/gorhill/uBlock/9367a6015b8cbb6b49347b00a105aab8f24df861/assets/resources/scriptlets.js
 // And https://github.com/bogachenko/presstheattack/blob/personal/scriptlets.txt
 
 // These are things I've found that I believe to be useful
 // Mainly for my personal use but feel free to use it
+
+// https://github.com/gorhill/uBlock/issues/1228
+/// window.name-defuser.js
+/// alias wnd.js
+(function() {
+    if ( window === window.top ) {
+        window.name = '';
+    }
+})();
